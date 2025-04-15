67687728f9b9b79cee0e1d063705893c6c41e9c7
Benjalius <benjaminpoblete141@gmail.com>
2025-04-15 12:22:08 -0400

    Mis archivos

---
+++ suma.py
@@ -0,0 +1,9 @@
+# n1 = 1
+# n2 = 2
+# # print(n1 + n2)
+
+print("ingrese 2 numeros")
+n1=int(input())
+n2=int(input())
+print(n1+n2)
+print("el resultado es", n1+n2)
---
+++ variable.py
@@ -0,0 +1,4 @@
+print("ingrese su nombre")
+nombre = "nigga"
+edad = 34
+print("hola", nombre, "y su edad es", edad)
