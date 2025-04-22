e33f2e5e253374526d61b6d3b36d1e7e677ea8df
benjalius <benjaminpoblete141@gmail.com>
2025-04-22 11:46:31 -0400

    si

--- Promedio.py
+++ Promedio.py
@@ -27,13 +27,13 @@
 
 #ingrese 3 numeros y muestre el mayor de ellos
 
-n1 = int(input("Ingrese el primer numero"))
-n2 = int(input("Ingrese el segundo numero"))
-n3 = int(input("Ingrese el tercer numero"))
+# n1 = int(input("Ingrese el primer numero"))
+# n2 = int(input("Ingrese el segundo numero"))
+# n3 = int(input("Ingrese el tercer numero"))
 
-if n1>n2 and n1>n3:
-    print(" El numero ", n1, "es el mayor")
-elif n2>n3:
-    print(" El numero ", n2, "es el mayor")
-else:
-    print(" El numero ", n3, "es el mayor ")
+# if n1>n2 and n1>n3:
+#     print(" El numero ", n1, "es el mayor")
+# elif n2>n3:
+#     print(" El numero ", n2, "es el mayor")
+# else:
+#     print(" El numero ", n3, "es el mayor ")
--- for.py
+++ for.py
@@ -1,11 +1,37 @@
 #pedir al usuario 3 notas y sacar promedio con for
 
-cant = int(input("Ingrese la cantidad de notas: "))
+# cant = int(input("Ingrese la cantidad de notas: "))
+# suma = 0
+
+# for i in range(cant):
+#     n1 = float(input("Ingrese la nota: "))
+#     suma = suma + n1
+
+# promedio = suma / cant
+# print("El promedio es: ", round(promedio))
+
+# designar 2 cantidades. preguntar la cantidad de votantes
+# por cada votante debe preguntar por quien votará
+# contar votos y mostrar resultados
+# definir ganador
+
+
+
+#cantidad de notas y promediarlas
+
+print("Ingresar cantidad de notas")
 suma = 0
+cantN=int(input())
+for i in range(cantN):
+    print("Ingrese la nota ",i +1)
+    nota=float(input())
+    # suma+=nota
+    suma=suma+nota
 
-for i in range(cant):
-    n1 = float(input("Ingrese la nota: "))
-    suma = suma + n1
+prom=suma/cantN
+print("Su promedio es ", prom)
 
-promedio = suma / cant
-print("El promedio es: ", round(promedio))
+if prom >=4:
+     print("El alumno aprueba")
+else:
+     print("El alumno reprueba")    
